f2933a3f8ac07101750313483248d06b8871cf68
Nahuel2998 <nahuel2998@gmail.com>
2026-08-20 00:58:05 -0300

    Refactor build.zig

--- build.zig
+++ build.zig
@@ -1,39 +1,76 @@
 const std = @import("std");
 
+const BuildContext = struct {
+    b:       *std.Build,
+    target:   std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+};
+
 pub fn build(b: *std.Build) void {
-    const target   = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const ctx: BuildContext = .{
+        .b        = b,
+        .target   = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
+    };
+
+    const xcb = buildXcb(ctx);
+    const x11 = buildLib(ctx, &.{
+        .{ .name = "xcb", .module = xcb },
+    });
+
+    buildCli(ctx, &.{
+        .{ .name = "x11", .module = x11 },
+    });
+}
+
+fn buildXcb(ctx: BuildContext) *std.Build.Module {
+    const b = ctx.b;
 
     const xcb = b.addTranslateC(.{
         .root_source_file = b.path("src/xcb.h"),
-        .target           = target,
-        .optimize         = optimize,
+        .target           = ctx.target,
+        .optimize         = ctx.optimize,
     });
     xcb.linkSystemLibrary("xcb", .{});
 
-    const x11 = b.createModule(.{
+    return xcb.createModule();
+}
+
+fn buildLib(ctx: BuildContext, imports: []const std.Build.Module.Import) *std.Build.Module {
+    const b = ctx.b;
+
+    const lib = b.createModule(.{
         .root_source_file = b.path("src/x11.zig"),
-        .target           = target,
-        .optimize         = optimize,
-        .imports          = &.{
-            .{ .name = "xcb", .module = xcb.createModule() },
-        },
+        .target           = ctx.target,
+        .optimize         = ctx.optimize,
+        .imports          = imports,
     });
 
+    const tests = b.addTest(.{
+        .root_module = lib,
+    });
+    const run_tests = b.addRunArtifact(tests);
+    const test_step = b.step("test_lib", "Run lib tests");
+    test_step.dependOn(&run_tests.step);
+
+    return lib;
+}
+
+fn buildCli(ctx: BuildContext, imports: []const std.Build.Module.Import) void {
+    const b = ctx.b;
+
     const exe = b.addExecutable(.{
-        .name = "Copied",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target           = target,
-            .optimize         = optimize,
-            .imports          = &.{
-                .{ .name = "x11", .module = x11 },
-            },
+        .name = "copied",
+        .root_module = ctx.b.createModule(.{
+            .root_source_file = b.path("src/cli.zig"),
+            .target           = ctx.target,
+            .optimize         = ctx.optimize,
+            .imports          = imports,
         }),
     });
     b.installArtifact(exe);
 
-    const run_step = b.step("run", "Run the app");
+    const run_step = b.step("run_cli", "Run the CLI");
     const run_cmd = b.addRunArtifact(exe);
     run_step.dependOn(&run_cmd.step);
     run_cmd.step.dependOn(b.getInstallStep());
@@ -41,17 +78,10 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const exe_tests = b.addTest(.{
+    const tests = b.addTest(.{
         .root_module = exe.root_module,
     });
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    const x11_tests = b.addTest(.{
-        .root_module = x11,
-    });
-    const run_x11_tests = b.addRunArtifact(x11_tests);
-
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_exe_tests.step);
-    test_step.dependOn(&run_x11_tests.step);
+    const run_tests = b.addRunArtifact(tests);
+    const test_step = b.step("test_cli", "Run CLI tests");
+    test_step.dependOn(&run_tests.step);
 }
